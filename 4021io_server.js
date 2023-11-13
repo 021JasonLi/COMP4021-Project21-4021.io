d65d1e74bc411a955c334f52ee7c5513a32f90b7
Jason Li <thliai@connect.ust.hk>
2023-11-13 14:56:35 +0800

    Signin Done

--- 4021io_server.js
+++ 4021io_server.js
@@ -31,7 +31,122 @@ function containWordCharsOnly(text) {
     return /^\w+$/.test(text);
 }
 
-// TODO: Handle HTTP requests
+// Handle the /register endpoint
+app.post("/register", (req, res) => {
+    // Get the JSON data from the body
+    const { username, name, password } = req.body;
+
+    // Reading the users.json file
+    const users = JSON.parse(fs.readFileSync("./data/users.json"));
+
+    // Checking for the user data correctness
+    // 1. Username, name and password are not empty
+    if ((!username) || (!name) || (!password)) {
+        res.json({
+            status: "error",
+            error: "Username/name/password cannot be empty."
+        });
+        return;
+    }
+    // 2. The username contains only underscores, letters or numbers 
+    if (!containWordCharsOnly(username)) {
+        res.json({
+            status: "error",
+            error: "Username contains invalid characters."
+        });
+        return;
+    }
+    // 3. The username does not exist in the current list of users 
+    if (username in users) {
+        res.json({
+            status: "error",
+            error: "Username has already been used."
+        });
+        return;
+    }
+
+    // Adding the new user account
+    const hash = bcrypt.hashSync(password, 10);
+
+    // Saving the users.json file
+    users[username] = { name, password: hash };
+    fs.writeFileSync("./data/users.json", JSON.stringify(users, null, "  "));
+
+    // Sending a success response to the browser
+    res.json({ status: "success" });
+
+});
+
+// Handle the /signin endpoint
+app.post("/signin", (req, res) => {
+    // Get the JSON data from the body
+    const { username, password } = req.body;
+
+    // Reading the users.json file
+    const users = JSON.parse(fs.readFileSync("./data/users.json"));
+
+    // Checking for username/password
+    if (!(username in users)) {
+        res.json({
+            status: "error",
+            error: "Incorrect username/password."
+        });
+        return;
+    }
+
+    const hashedPassword = users[username].password;
+    if (!bcrypt.compareSync(password, hashedPassword)) {
+        res.json({
+            status: "error",
+            error: "Incorrect username/password."
+        });
+        return;
+    }
+
+    // Sending a success response with the user account
+    const output = { 
+        username,
+        name: users[username].name
+    };
+    req.session.user = output;
+    res.json({
+        status: "success",
+        user: output
+    });
+
+});
+
+// Handle the /validate endpoint
+app.get("/validate", (req, res) => {
+    // Getting req.session.user
+    const user = req.session.user;
+    if (!user) {
+        res.json({
+            status: "error",
+            error: "Session expired.",
+        });
+        return;
+    }
+
+    // Sending a success response with the user account
+    res.json({
+        status: "success",
+        user
+    });
+ 
+});
+
+// Handle the /signout endpoint
+app.get("/signout", (req, res) => {
+    // Deleting req.session.user
+    delete req.session.user;
+    
+    // Sending a success response
+    res.json({
+        status: "success"
+    });
+
+});
 
 // Create the socket server
 const httpServer = createServer(app);
